d355a41025136b8c7eba612c6f7b468ad6187bb2
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-15 15:31:43 +0200

    Add development combat HUD shell

---
+++ Source/LostsenseGame/Public/LostsenseDevelopmentHUD.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "LostsenseDevelopmentHUD.generated.h"
+
+UCLASS()
+class LOSTSENSEGAME_API ALostsenseDevelopmentHUD final : public AHUD {
+  GENERATED_BODY()
+
+public:
+  virtual void DrawHUD() override;
+
+private:
+  void DrawMeter(const FString &Label, double Current, double Maximum, float X,
+                 float Y, float Width) const;
+};
